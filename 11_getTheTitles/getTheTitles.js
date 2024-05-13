d6356824e65a3171ee4429ecc44c2d930a2b1645
Ashvin <ashvinjangid10@gmail.com>
2024-05-13 19:57:44 +0530

    refactor exercise getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,13 +1,5 @@
 const getTheTitles = function(books) {
-    let titles = []
-    books.forEach(book => {
-        titles.push(book['title'])
-    });
-    return titles
-
-    // Another short way to do this is to use a map function
-    //* return books.map((book) => book.title);
-
+    return books.map((book) => book.title)
 };
 
 // Do not edit below this line
